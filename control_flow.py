bc36ef1a83254ddbb73ce246b8e19cbb2cb99877
Tuan Pham <TuanPham@Tuans-MacBook-Pro.local>
2021-05-20 11:29:35 +0100

    control_flow added

--- control_flow.py
+++ control_flow.py
@@ -5,7 +5,7 @@ weather = "sunny"
 if weather == "sunny": # If this condition is 'True' execute the next line of code
     print("Enjoy the weather! ") # If 'True' this line will be executed
 elif weather == "raining":
-    print("Take a umbrella")
+    print("Take a umbrella!")
 else:
     print("Oops sorry something went wrong, please try again! ") # If 'False' this line will be executed
 # Add a condition to use elif when the condition is 'False'
